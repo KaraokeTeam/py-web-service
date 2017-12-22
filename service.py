e230de0a83a5f46a7b550ff35bff963916c36fb7
Or Priesender <orpris12@gmail.com>
2017-12-22 21:51:25 +0200

    added basic web service

---
+++ service.py
@@ -0,0 +1,30 @@
+import grouper
+from flask import Flask, jsonify, request, Response
+from werkzeug import utils
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Karaoke Team'
+
+
+@app.route('/group', methods=['POST'])
+def get_group():
+    file = request.files['file']
+    file.save(utils.secure_filename(file.filename))
+    groups = grouper.get_note_groups(file.filename)
+    return Response(jsonify(groups), status=200, mimetype='application/json')
+
+
+@app.route('/grade', methods=['POST'])
+def get_grade():
+    file = request.files['file']
+    file.save(utils.secure_filename(file.filename))
+    # algorithm works
+    return Response("{'grade':'100'}", status=200, mimetype='application/json')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
