fa890eab878f91020cd4bc1c528d8be9e0e0638a
Or Priesender <orpris12@gmail.com>
2018-01-04 10:24:48 +0200

    added venv

---
+++ venv/Scripts/aubiocut-script.py
@@ -0,0 +1,12 @@
+#!c:\dev\python~1\py-web~1\venv\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'aubio==0.4.6','console_scripts','aubiocut'
+__requires__ = 'aubio==0.4.6'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('aubio==0.4.6', 'console_scripts', 'aubiocut')()
+    )
